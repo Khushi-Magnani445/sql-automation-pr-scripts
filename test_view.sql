b29f83ba84f87ee26cbd0949bfb22a47f1369182
Khushi-Magnani445 <khushimagnani9106@gmail.com>
2026-04-07 13:09:24 +0530

    Update test view with new test view 2

--- test_view.sql
+++ test_view.sql
@@ -1,3 +1,2 @@
 CREATE VIEW test_view AS SELECT id, name FROM users;
 CREATE VIEW test_view AS SELECT id, name, email FROM users;
-CREATE VIEW test_view AS SELECT id, name, email, department, salary,address,phone,bonus FROM users;
