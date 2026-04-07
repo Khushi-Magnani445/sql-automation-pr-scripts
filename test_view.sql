90ee6ea7eb155bed0b030123e6986a865ca29fbf
Khushi-Magnani445 <khushimagnani9106@gmail.com>
2026-04-07 13:16:19 +0530

    Demonstrating fully detailed mapping changes

--- test_view.sql
+++ test_view.sql
@@ -1,2 +1,3 @@
 CREATE VIEW test_view AS SELECT id, name FROM users;
 CREATE VIEW test_view AS SELECT id, name, email FROM users;
+CREATE VIEW advanced_employee_view AS SELECT id AS employee_id, name AS full_name, department, location, manager_id FROM users;
