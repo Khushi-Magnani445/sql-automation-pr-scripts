b17eb25c4e353200e611fe7104e7057f3fb585cb
Khushi-Magnani445 <khushimagnani9106@gmail.com>
2026-04-07 13:27:33 +0530

    Demonstrate explicit column removal perfectly!

--- test_view.sql
+++ test_view.sql
@@ -1,3 +1,1 @@
-CREATE VIEW test_view AS SELECT id, name FROM users;
-CREATE VIEW test_view AS SELECT id, name, email FROM users;
 CREATE VIEW test_view AS SELECT id AS employee_id, name AS full_name, department, location, manager_id FROM users;
