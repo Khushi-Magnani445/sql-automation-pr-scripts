46d03d389b4357720dee03e0ed52da0269904033
Khushi-Magnani445 <khushimagnani9106@gmail.com>
2026-04-07 12:00:07 +0530

    Added salary and address

--- test_view.sql
+++ test_view.sql
@@ -1,3 +1,3 @@
 CREATE VIEW test_view AS SELECT id, name FROM users;
 CREATE VIEW test_view AS SELECT id, name FROM users;
-CREATE VIEW test_view AS SELECT id, name, email, department FROM users;
+CREATE VIEW test_view AS SELECT id, name, email, department, salary, address FROM users;
